c656bea658990cbcab09066fa3e83f2bf380b921
Finomnis <finomnis@gmail.com>
2021-11-19 14:04:22 +0100

    Implement shutdown result collection

--- src/exit_state.rs
+++ src/exit_state.rs
@@ -19,5 +19,34 @@ pub fn join_shutdown_results(
     left: ShutdownResults,
     right: Vec<ShutdownResults>,
 ) -> ShutdownResults {
-    unimplemented!()
+    let mut has_error = false;
+
+    let mut result = match left {
+        Ok(r) => r,
+        Err(r) => {
+            has_error = true;
+            r
+        }
+    };
+
+    let right_unwrapped = right
+        .into_iter()
+        .map(|el| match el {
+            Ok(r) => r,
+            Err(r) => {
+                has_error = true;
+                r
+            }
+        })
+        .collect::<Vec<_>>();
+
+    for mut right_element in right_unwrapped {
+        result.append(&mut right_element);
+    }
+
+    if has_error {
+        Err(result)
+    } else {
+        Ok(result)
+    }
 }
