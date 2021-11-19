dd5f3494b757e622d78bd492c942192860f3af8f
Finomnis <finomnis@gmail.com>
2021-11-19 02:17:31 +0100

    Added waiting for subsystems to shutdown

--- src/subsystem.rs
+++ src/subsystem.rs
@@ -2,6 +2,7 @@ use std::sync::Arc;
 
 use anyhow::Result;
 use async_trait::async_trait;
+use futures::future::try_join_all;
 use tokio::sync::RwLock;
 use tokio::task::JoinHandle;
 
@@ -11,7 +12,7 @@ use crate::shutdown_token::ShutdownToken;
 pub struct SubsystemData {
     name: String,
     subsystems: RwLock<Vec<Arc<SubsystemData>>>,
-    subsystem_joinhandles: RwLock<Vec<Arc<JoinHandle<()>>>>,
+    subsystem_joinhandles: RwLock<Vec<JoinHandle<()>>>,
     shutdown_token: ShutdownToken,
 }
 
@@ -30,17 +31,16 @@ impl SubsystemData {
         }
     }
 
-    pub async fn add_subsystem(
-        &self,
-        subsystem: Arc<SubsystemData>,
-        joinhandle: Arc<JoinHandle<()>>,
-    ) {
+    pub async fn add_subsystem(&self, subsystem: Arc<SubsystemData>, joinhandle: JoinHandle<()>) {
         self.subsystem_joinhandles.write().await.push(joinhandle);
         self.subsystems.write().await.push(subsystem);
     }
 
     pub async fn perform_shutdown(&self) -> Result<()> {
-        Ok(())
+        match try_join_all(self.subsystem_joinhandles.write().await.iter_mut()).await {
+            Ok(_) => Ok(()),
+            Err(err) => Err(anyhow::anyhow!(err)),
+        }
     }
 }
 
@@ -66,11 +66,11 @@ impl SubsystemHandle {
         let subsystem_handle = SubsystemHandle::new(new_subsystem.clone());
 
         // Spawn new task
-        let join_handle = Arc::new(tokio::spawn(run_subsystem(
+        let join_handle = tokio::spawn(run_subsystem(
             self.data.name.clone() + name,
             subsystem,
             subsystem_handle,
-        )));
+        ));
 
         // Store subsystem data
         self.data.add_subsystem(new_subsystem, join_handle).await;
@@ -86,45 +86,6 @@ impl SubsystemHandle {
         self.shutdown_token.wait_for_shutdown().await
     }
 }
-
-// impl SubsystemHandle {
-//     pub fn new(subsystem: Box<dyn AsyncSubsystem>, shutdown_token: ShutdownToken) -> Self {
-//         Self {
-//             subsystem,
-//             shutdown_token,
-//         }
-//     }
-
-//     pub fn start<S: AsyncSubsystem + 'static + Send>(
-//         &mut self,
-//         name: &'static str,
-//         subsystem: S,
-//     ) -> &mut Self {
-//         let boxed_subsys = Box::new(subsystem);
-//         let shutdown_token = self.shutdown_token.clone();
-
-//         tokio::spawn(async move {
-//             let handle = Box::new(SubsystemHandle::new(boxed_subsys, shutdown_token));
-//             handle.run();
-//         });
-
-//         self
-//     }
-
-//     pub async fn on_shutdown_request(&self) {
-//         self.shutdown_token.wait_for_shutdown().await
-//     }
-
-//     pub fn initiate_shutdown(&self) {
-//         self.shutdown_token.shutdown();
-//     }
-
-//     pub fn shutdown_token(&self) -> ShutdownToken {
-//         self.shutdown_token.clone()
-//     }
-
-//     fn run(mut self) {}
-// }
 
 #[async_trait]
 pub trait AsyncSubsystem {
