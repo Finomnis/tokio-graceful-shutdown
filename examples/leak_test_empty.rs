3b44caab48f229737b9582a2c02d4310fc82dce1
Finomnis <finomnis@gmail.com>
2021-11-22 23:15:22 +0100

    Force leak test reference example to leak

--- examples/leak_test_empty.rs
+++ examples/leak_test_empty.rs
@@ -1,14 +1,6 @@
 //! This example is not an actual example.
 //!
-//! It is just a demonstrator to show that this crate does not leak memory.
-//! It gets used by the CI to perform a very crude leak check.
-//!
-//! Run this example with the environment variable:
-//!     sudo apt install valgrind
-//!     cargo build --example leak_test
-//!     valgrind --leak-check=yes target/debug/examples/leak_test
-//!
-//! This will print allocation information, including the amount of leaked memory.
+//! It is the counterpart to leak_test
 
 use anyhow::Result;
 use env_logger::{Builder, Env};
@@ -16,6 +8,9 @@ use tokio::time::{sleep, Duration};
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let xs = vec![0, 1, 2, 3];
+    std::mem::forget(xs);
+
     // Init logging
     Builder::from_env(Env::default().default_filter_or("debug")).init();
 
