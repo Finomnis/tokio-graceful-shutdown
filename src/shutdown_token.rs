a01a9bdc52ce153f244b2ca1fd9de0e53051ed67
Finomnis <finomnis@gmail.com>
2021-11-28 15:03:25 +0100

    Add partial_shutdown to shutdown_token

--- src/shutdown_token.rs
+++ src/shutdown_token.rs
@@ -20,6 +20,13 @@ impl ShutdownToken {
         }
     }
 
+    pub fn partial_shutdown(&self) {
+        if !self.token.is_cancelled() {
+            log::debug!("Initiating partial shutdown ...");
+            self.token.cancel()
+        }
+    }
+
     pub async fn wait_for_shutdown(&self) {
         self.token.cancelled().await
     }
@@ -61,6 +68,35 @@ mod tests {
             assert!(!token2.is_shutting_down());
 
             token1.shutdown();
+            sleep(Duration::from_millis(100)).await;
+
+            assert!(finished.load(Ordering::SeqCst));
+            assert!(token1.is_shutting_down());
+            assert!(token2.is_shutting_down());
+        };
+
+        tokio::join!(stopper, stoppee);
+    }
+
+    #[tokio::test]
+    async fn triggers_correctly_on_partial() {
+        let finished = AtomicBool::new(false);
+
+        let token1 = create_shutdown_token();
+        let token2 = token1.clone();
+
+        let stoppee = async {
+            token2.wait_for_shutdown().await;
+            finished.store(true, Ordering::SeqCst);
+        };
+
+        let stopper = async {
+            sleep(Duration::from_millis(100)).await;
+            assert!(!finished.load(Ordering::SeqCst));
+            assert!(!token1.is_shutting_down());
+            assert!(!token2.is_shutting_down());
+
+            token1.partial_shutdown();
             sleep(Duration::from_millis(100)).await;
 
             assert!(finished.load(Ordering::SeqCst));
