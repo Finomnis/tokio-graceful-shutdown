46c99ed5a50520e74853061ab79da40736a798b5
Finomnis <finomnis@gmail.com>
2021-11-28 20:15:34 +0100

    Revert "Test signal based shutdown"
    This reverts commit 5ff616bbcb9bba36ccf842f01f12b66d91a488a7.

--- src/signal_handling.rs
+++ src/signal_handling.rs
@@ -1,5 +1,5 @@
 /// Waits for a signal that requests a graceful shutdown, like SIGTERM or SIGINT.
-#[cfg(all(unix, not(test)))]
+#[cfg(unix)]
 async fn wait_for_signal_impl() {
     use tokio::signal::unix::{signal, SignalKind};
 
@@ -13,7 +13,7 @@ async fn wait_for_signal_impl() {
 }
 
 /// Waits for a signal that requests a graceful shutdown, Ctrl-C (SIGINT).
-#[cfg(all(windows, not(test)))]
+#[cfg(windows)]
 async fn wait_for_signal_impl() {
     use tokio::signal::ctrl_c;
 
@@ -21,60 +21,9 @@ async fn wait_for_signal_impl() {
     log::debug!("Received SIGINT.");
 }
 
-#[cfg(test)]
-use std::sync::atomic::{AtomicBool, Ordering};
-
-#[cfg(test)]
-static TRIGGER: AtomicBool = AtomicBool::new(false);
-
-#[cfg(test)]
-pub fn trigger_signal() {
-    TRIGGER.store(true, Ordering::SeqCst);
-}
-
-#[cfg(test)]
-async fn wait_for_signal_impl() {
-    while !TRIGGER.load(Ordering::SeqCst) {
-        tokio::time::sleep(tokio::time::Duration::from_millis(10)).await;
-    }
-}
-
 /// Registers Ctrl+C and SIGTERM handlers to cause a program shutdown.
 /// Further, registers a custom panic handler to also initiate a shutdown.
 /// Otherwise, a multi-threaded system would deadlock on panik.
 pub async fn wait_for_signal() {
     wait_for_signal_impl().await
-}
-
-#[cfg(test)]
-mod test {
-    use tokio::time::{sleep, Duration};
-
-    use crate::{SubsystemHandle, Toplevel};
-
-    #[tokio::test]
-    async fn shutdown_through_signal() {
-        env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("off")).init();
-
-        let subsystem = |subsys: SubsystemHandle| async move {
-            subsys.on_shutdown_requested().await;
-            sleep(Duration::from_millis(200)).await;
-            Ok(())
-        };
-
-        let toplevel = Toplevel::new().catch_signals();
-        tokio::join!(
-            async {
-                sleep(Duration::from_millis(100)).await;
-                super::trigger_signal();
-            },
-            async {
-                let result = toplevel
-                    .start("subsys", subsystem)
-                    .handle_shutdown_requests(Duration::from_millis(400))
-                    .await;
-                assert!(result.is_ok());
-            },
-        );
-    }
 }
