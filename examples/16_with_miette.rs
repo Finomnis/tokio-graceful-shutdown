ba0c2c257c48e71052d03ef96e7ff3038f427bc7
Finomnis <finomnis@gmail.com>
2022-04-03 13:08:51 +0200

    Add miette example. Depends on https://github.com/zkat/miette/pull/149

---
+++ examples/16_with_miette.rs
@@ -0,0 +1,29 @@
+//! This example shows how to use this library with miette instead of anyhow
+
+use env_logger::{Builder, Env};
+use miette::{miette, IntoDiagnostic, Result};
+use tokio::time::{sleep, Duration};
+use tokio_graceful_shutdown::{GracefulShutdownError, SubsystemHandle, Toplevel};
+
+async fn subsys1(_subsys: SubsystemHandle) -> Result<()> {
+    log::info!("Subsystem1 started.");
+    sleep(Duration::from_millis(500)).await;
+    log::info!("Subsystem1 stopped.");
+
+    // Task ends with an error. This should cause the main program to shutdown.
+    Err(miette!("Subsystem1 threw an error."))
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Init logging
+    Builder::from_env(Env::default().default_filter_or("debug")).init();
+
+    // Create toplevel
+    Toplevel::new()
+        .start("Subsys1", subsys1)
+        .catch_signals()
+        .handle_shutdown_requests::<GracefulShutdownError>(Duration::from_millis(1000))
+        .await
+        .into_diagnostic()
+}
