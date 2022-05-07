1fd294c326667da7668a5451900fca68c3bee324
Finomnis <finomnis@gmail.com>
2022-05-07 11:39:31 +0200

    Silence missing code example for ErrTypeTraits

--- src/lib.rs
+++ src/lib.rs
@@ -92,8 +92,9 @@
 
 type BoxedError = Box<dyn std::error::Error + Send + Sync + 'static>;
 
+#[allow(rustdoc::missing_doc_code_examples)]
 /// A collection of traits a custom error has to fulfill in order to be
-/// usable as the `ErrType` generic of [Toplevel].
+/// usable as the `ErrType` of [Toplevel].
 pub trait ErrTypeTraits:
     std::fmt::Debug + std::fmt::Display + 'static + Send + Sync + Sized
 {
