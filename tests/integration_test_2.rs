dc2bc9e1b1c36bcf3d2580ca4670ad7445e85a78
Finomnis <finomnis@gmail.com>
2023-10-22 16:13:46 +0200

    Add wait_for_children test

--- tests/integration_test_2.rs
+++ tests/integration_test_2.rs
@@ -9,6 +9,8 @@ use std::{
     sync::{Arc, Mutex},
 };
 
+use crate::common::Event;
+
 /// Wrapper function to simplify lambdas
 type BoxedError = Box<dyn Error + Sync + Send>;
 type BoxedResult = Result<(), BoxedError>;
@@ -41,4 +43,57 @@ async fn leak_subsystem_handle() {
     assert!(logs_contain(
         "The SubsystemHandle object must not be leaked out of the subsystem!"
     ));
+}
+
+#[tokio::test]
+#[traced_test]
+async fn wait_for_children() {
+    let (nested1_started, set_nested1_started) = Event::create();
+    let (nested1_finished, set_nested1_finished) = Event::create();
+    let (nested2_started, set_nested2_started) = Event::create();
+    let (nested2_finished, set_nested2_finished) = Event::create();
+
+    let nested_subsys2 = move |subsys: SubsystemHandle| async move {
+        set_nested2_started();
+        subsys.on_shutdown_requested().await;
+        sleep(Duration::from_millis(100)).await;
+        set_nested2_finished();
+        BoxedResult::Ok(())
+    };
+
+    let nested_subsys1 = move |subsys: SubsystemHandle| async move {
+        subsys.start(SubsystemBuilder::new("nested2", nested_subsys2));
+        set_nested1_started();
+        subsys.on_shutdown_requested().await;
+        sleep(Duration::from_millis(100)).await;
+        set_nested1_finished();
+        BoxedResult::Ok(())
+    };
+
+    let subsys1 = move |mut subsys: SubsystemHandle| async move {
+        subsys.start(SubsystemBuilder::new("nested1", nested_subsys1));
+
+        sleep(Duration::from_millis(100)).await;
+
+        subsys.request_shutdown();
+
+        assert!(nested1_started.get());
+        assert!(!nested1_finished.get());
+        assert!(nested2_started.get());
+        assert!(!nested2_finished.get());
+
+        subsys.wait_for_children().await;
+
+        assert!(nested1_finished.get());
+        assert!(nested2_finished.get());
+
+        BoxedResult::Ok(())
+    };
+
+    Toplevel::new(|s| async move {
+        s.start(SubsystemBuilder::new("subsys", subsys1));
+    })
+    .handle_shutdown_requests(Duration::from_millis(500))
+    .await
+    .unwrap();
 }
